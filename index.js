0bb414b9e573a8eaf0f7a82300744770e8c19395
Cristian Incarnato <cristian.cdi@gmail.com>
2021-02-25 23:25:45 -0300

    solved

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const findIntersection = function(strArr){
 
     /* YOUR CODE HERE */
-    return strArr;
+    let arr1 = strArr[0].split(", ")
+    let arr2 = strArr[1].split(", ")
+
+    let intersectionArr = arr1.filter(n=> {
+        if(arr2.includes(n)){
+            return  true
+        }
+        return false
+    })
+
+    if(intersectionArr.length == 0) return 'false'
+
+    return intersectionArr.join(",")
 
 }
 
